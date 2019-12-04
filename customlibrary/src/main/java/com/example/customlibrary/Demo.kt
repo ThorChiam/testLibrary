1bc7cee4c4a14cc3ea0fa7e0eb667e04c452966a
Stever Zhan <zhanshentuo@bigo.sg>
2019-12-04 10:25:09 +0800

    added library

---
+++ customlibrary/src/main/java/com/example/customlibrary/Demo.kt
@@ -0,0 +1,23 @@
+package com.example.customlibrary
+
+//import android.view.View
+//import bg.devlabs.transitioner.Transitioner
+//import com.downloader.PRDownloader
+//import com.downloader.Status
+import com.yhao.floatwindow.FloatWindow
+import com.yhao.floatwindow.IFloatWindow
+
+object Demo {
+
+    fun demoCommon(): IFloatWindow {
+        return FloatWindow.get()
+    }
+
+    fun demo1() {
+//                val transitioner = Transitioner
+    }
+
+//    fun demo2():Status {
+//        return PRDownloader.getStatus(0)
+//    }
+}
